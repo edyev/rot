e973fa7cfdd3e062a02fd0088037321583b49167
Eddie Vazquez <lalo_psp@hotmail.com>
2018-06-13 12:55:18 -0500

    Update Sigfox.h

--- Sigfox.h
+++ Sigfox.h
@@ -1,6 +1,22 @@
 #ifndef Sigfox_h
 #define Sigfox_h
 
+#include "WISOL.h"
+
+class SensorSig{
+public:
+  SensorSigfox();
+  ~SensorSigfox();
+  void testConnection();
+  void setBehaviour();
+  void setBehaviour(uint8_t sensor_type, uint8_t description);
+  void sendValue();
+  
+  
+  
+private:
+  
+}
 
 
 #endif
